aff1201d460d280311855ea3dac1d97405c9f8f2
mamadouthierno <142215471+mamadouthierno@users.noreply.github.com>
2025-04-22 21:23:47 +0200

    Update __init__.py

--- onglets/__init__.py
+++ onglets/__init__.py
@@ -1,7 +1,5 @@
-from onglets.contact import contact
-from onglets.accueil import accueil
-from onglets.apropos import a_propos
-from onglets.prediction import modelisation
-from onglets.analyse import analyse_descriptive
-
-
+from .contact import contact
+from .accueil import accueil
+from .apropos import a_propos
+from .prediction import modelisation
+from .analyse import analyse_descriptive
